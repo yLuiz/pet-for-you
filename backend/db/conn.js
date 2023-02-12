8d938cbff2403c2a6a55855cd9ac9d6f3d390f1b
yluiz <luizvictor1231@gmail.com>
2023-02-12 11:45:28 -0300

    MongoDB connection fixing

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const MONGODB_URL = process.env.MONGODB_URL;
 
 async function main() {
-  await mongoose.connect(MONGODB_URL);
+  await mongoose.connect(MONGODB_URL, {
+    dbName: 'adopt_pet'
+  });
   console.log("Connected to Mongoose!"); 
 }
 
