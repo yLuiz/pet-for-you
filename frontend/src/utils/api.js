b8c1632151a4aef9b01c0741dd048bd15bc6450b
yluiz <luizvictor1231@gmail.com>
2023-02-12 12:09:22 -0300

    Adicionando o environment correto no AXIOS

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+import environment from "../environment/environment";
 
 export default axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: environment.REACT_APP_API,
 });
