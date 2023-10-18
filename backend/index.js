76ad3b0438c966f21181f2b0cca7137c782d0da3
yluiz <luizvictor1231@gmail.com>
2023-10-18 15:19:34 -0400

    refactor: mudando console.log 'APP IS RUNNING'

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,16 @@ server.use('/pets', PetRoutes);
 
 
 server.listen(process.env.PORT || 5001, () => {
+
+  if (process.env.PORT) {
+    console.log(`
+      ==============================================
+      🚀 API IS RUNNING IN PORT ${ process.env.PORT } 🚀
+      ==============================================
+    `);
+    return;
+  }
+
   console.log(`
     ==============================================
     🚀 API IS RUNNING IN http://localhost:5001/ 🚀
